3d2fd29e9b1e1914d614196b36bff779fec9a18e
Dongineer <30032dongsu@gmail.com>
2019-06-26 16:01:38 +0900

    [수정] appspec 주석 추가, deploy.sh 환경변수 추가 및 명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 source /home/ec2-user/.bash_profile
+export WILLSON=$HOME/deploy/willson
 
-NODE_ENV=src
-pm2 start /home/ec2-user/deploy/willson/config/dist/bundle.js
+ NODE_ENV=src
+ pm2 delete willson
+ pm2 start --name willson $WILLSON/config/dist/bundle.js
+
+ 
